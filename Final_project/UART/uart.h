8a9f4548b16836bcadf3cfb8714778c1aa517316
fyder9 <gossettifederico778@gmail.com>
2026-06-20 12:54:17 +0200

    cleaned uart header file - uart.c Send part still TODO

--- Final_project/UART/uart.h
+++ Final_project/UART/uart.h
@@ -4,12 +4,10 @@
 #define FCY 72000000UL
 
 void UART1_SendChar(unsigned c);
-char UART1_ReceiveChar(void);
+char UART1_ReadChar(void);
 int  UART1_HasData(void);
 void UART1_Init(void);
 void UART1_SendString(const char* str);
 char* UART1_ReceiveString(char* buffer, int maxLength);
-
-extern volatile int uart_rx_overflow;
 
 #endif // UART_H
